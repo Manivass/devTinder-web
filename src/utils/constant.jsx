67325d87865cad078b5fe51ea257e9f26ee0cf3d
Manivass <manivassv@gmail.com>
2026-03-24 20:22:31 +0530

    fixed bug

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,1 +1,1 @@
-export const BASE_URL = location.host === "localhost:5173" ? "http://localhost:4000" : "/api ";
+export const BASE_URL = location.host === "localhost:5173" ? "http://localhost:4000" : "/api";
